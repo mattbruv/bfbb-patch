74af7aafb7d3b06e9e48aef7694f660c90f49edc
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-12-23 17:15:37 -0500

    open object files, write out file, setup args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use argh::FromArgs;
+use object;
+use std::{error::Error, fs};
+
+#[derive(FromArgs)]
+/// Creates a patched ELF object file. Patches code from source object into target object.
+struct PatchArgs {
+    /// source object file
+    #[argh(positional)]
+    source: String,
+
+    /// target object file
+    #[argh(positional)]
+    target: String,
+
+    /// out object file
+    #[argh(positional, short = 'o')]
+    out: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: PatchArgs = argh::from_env();
+    let target_bin = fs::read(args.target)?;
+    let target_elf = object::File::parse(&*target_bin)?;
+    let source_bin = fs::read(args.source)?;
+    let source_elf = object::File::parse(&*source_bin)?;
+
+    //let patch_elf = object::write::
+    let out_elf = object::write::Object::new(
+        object::BinaryFormat::Elf,
+        object::Architecture::PowerPc,
+        object::Endianness::Little,
+    );
+
+    fs::write(args.out, out_elf.write().unwrap())?;
+
+    Ok(())
 }
