329e32622489849a04295ce1580abdfaff86a01a
Matt P <5638426+mattbruv@users.noreply.github.com>
2022-12-25 17:49:29 -0500

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use argh::FromArgs;
-use object::{self, read::elf::Sym, File, Object, ObjectSection, ObjectSymbol, Symbol, SymbolKind};
-use std::{collections::binary_heap::Iter, error::Error, fs};
+use object::{
+    self, read::elf::Sym, File, Object, ObjectSection, ObjectSymbol, Relocation, SectionKind,
+    Symbol, SymbolKind,
+};
+use std::{collections::binary_heap::Iter, error::Error, fs, io::Read};
 
 #[derive(FromArgs)]
 /// Creates a patched ELF object file. Patches code from source object into target object.
@@ -53,10 +56,43 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect();
 
     for sym in replace_funcs {
-        println!("{}, {}", sym.name().unwrap(), sym.address());
+        let name = sym.name().unwrap();
+        let bytes_target = get_function_code(name, &target_elf);
+        let bytes_source = get_function_code(name, &source_elf);
     }
 
     fs::write(args.out, out_elf.write().unwrap())?;
 
     Ok(())
+}
+
+fn bytes_equal(vec1: &Vec<u8>, vec2: &Vec<u8>) -> bool {
+    if !vec1.len() != vec2.len() {
+        return false;
+    }
+
+    for i in 0..vec1.len() {
+        if vec1[i] != vec2[i] {
+            return false;
+        }
+    }
+
+    true
+}
+
+// Takes a function symbol + elf and returns the bytes of that function
+fn get_function_code(func_name: &str, elf: &object::read::File) -> Vec<u8> {
+    let sym = elf
+        .symbols()
+        .find(|s| s.name().unwrap() == func_name)
+        .unwrap();
+
+    let index = sym.section_index().unwrap();
+    let begin = sym.address() as usize;
+    let end = begin + sym.size() as usize;
+    let section = elf.section_by_index(index).unwrap();
+    let data = section.data().unwrap();
+    let bytes: Vec<u8> = data[begin..end].into();
+
+    bytes
 }
